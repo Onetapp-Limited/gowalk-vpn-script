cf950d35c5c677495fc75b89319d7f0097fbd066
vbashtyrev <31920099+vbashtyrev@users.noreply.github.com>
2024-09-10 12:33:50 +0300

    Update update.sh
    update url 
    https://github.com/AccountOnetapp/gowalk-vpn-script/edit/main

--- update.sh
+++ update.sh
@@ -3,11 +3,11 @@ CURRENT_VERSION=4 #PASTE FILE VERSION HERE AND IN SAME VARIABLE IN createvpn.sh
 
 COMMANDS=`
 #START COMMANDS
-wget -q --header='Authorization: token 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' https://raw.githubusercontent.com/gasabdullaev/vpn/HEAD/check.sh -O check.sh ; chmod +x check.sh
+wget 'https://github.com/AccountOnetapp/gowalk-vpn-script/edit/main/check.sh -O check.sh ; chmod +x check.sh
 crontab -r
 crontab -l | { cat; echo "* * * * * bash /root/check.sh >> vpn.log 2>&1"; } | crontab -
-crontab -l | { cat; echo "* * * * * wget -q --header='Authorization: token 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' https://raw.githubusercontent.com/gasabdullaev/vpn/HEAD/update.sh -O update.sh ; chmod +x update.sh ; bash update.sh >> update.log 2>&1"; } | crontab -
-wget -q --header='Authorization: token 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' https://raw.githubusercontent.com/gasabdullaev/vpn/HEAD/fix20230120.sh -O fix20230120.sh && chmod +x fix20230120.sh && bash fix20230120.sh
+crontab -l | { cat; echo "* * * * * wget 'https://github.com/AccountOnetapp/gowalk-vpn-script/edit/main/update.sh -O update.sh ; chmod +x update.sh ; bash update.sh >> update.log 2>&1"; } | crontab -
+wget 'https://github.com/AccountOnetapp/gowalk-vpn-script/edit/main/fix20230120.sh -O fix20230120.sh && chmod +x fix20230120.sh && bash fix20230120.sh
 #END COMMANDS
 `
 
