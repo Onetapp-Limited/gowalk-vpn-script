29ba86ab9fefa7e2a38cf166ecf79fe6f50dd868
vbashtyrev <31920099+vbashtyrev@users.noreply.github.com>
2024-12-13 12:28:37 +0300

    Update update.sh
    change link

--- update.sh
+++ update.sh
@@ -3,11 +3,11 @@ CURRENT_VERSION=4 #PASTE FILE VERSION HERE AND IN SAME VARIABLE IN createvpn.sh
 
 COMMANDS=`
 #START COMMANDS
-wget https://raw.githubusercontent.com/AccountOnetapp/gowalk-vpn-script/main/check.sh -O check.sh ; chmod +x check.sh
+wget https://raw.githubusercontent.com/Onetapp-Limited/gowalk-vpn-script/refs/heads/main/check.sh -O check.sh ; chmod +x check.sh
 crontab -r
 crontab -l | { cat; echo "* * * * * bash /root/check.sh >> vpn.log 2>&1"; } | crontab -
 crontab -l | { cat; echo "* * * * * wget https://raw.githubusercontent.com/AccountOnetapp/gowalk-vpn-script/main/update.sh -O update.sh ; chmod +x update.sh ; bash update.sh >> update.log 2>&1"; } | crontab -
-wget https://raw.githubusercontent.com/AccountOnetapp/gowalk-vpn-script/main/fix20230120.sh -O fix20230120.sh && chmod +x fix20230120.sh && bash fix20230120.sh
+wget https://raw.githubusercontent.com/Onetapp-Limited/gowalk-vpn-script/refs/heads/main/fix20230120.sh -O fix20230120.sh && chmod +x fix20230120.sh && bash fix20230120.sh
 #END COMMANDS
 `
 
